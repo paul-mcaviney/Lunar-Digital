5c0f6c00b2b2cb487a22b5b0a73b9cb6341833e2
Paul McAviney <87715498+paul-mcaviney@users.noreply.github.com>
2021-09-15 11:47:37 -0600

    trying to get the her section better

---
+++ scripts.js
@@ -0,0 +1,20 @@
+const deviceType = () => {
+    const ua = navigator.userAgent;
+
+    if (/tablet|ipad|playbook|silk|(android(?!.*mobi))/i.test(ua)) {
+        return "tablet";
+    }
+
+    else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
+        return "mobile";
+    }
+    return "desktop";
+};
+
+// window.alert(window.innerHeight);
+
+document.documentElement.style.setProperty('--window-height', window.innerHeight);
+
+// if (deviceType() === "desktop") {
+//     // add a class to the forest-trees
+// }
