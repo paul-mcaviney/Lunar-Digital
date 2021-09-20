6149f91b7bcfff197f4ad2f1f3d2052da3364e33
Paul McAviney <87715498+paul-mcaviney@users.noreply.github.com>
2021-09-20 17:00:00 -0600

    Working on questionnaire

--- scripts.js
+++ scripts.js
@@ -11,16 +11,38 @@ setWindowHeight();
 // const oldWebsite = document.getElementById("question-5-2");
 
 function oldNewCheck() {
-    const ifActive = document.getElementById("if-checked");
+    const ifActive = document.getElementById("improve-website-div");
     const newWebsite = document.getElementById("question-5-1");
     const oldWebsite = document.getElementById("question-5-2");
 
 
     if (oldWebsite.checked) {
         ifActive.style.display = "block";
-        ifActive.style.transition = "0.4s";
     } else if (newWebsite.checked) {
         ifActive.style.display = "none";
+    }
+}
+
+function existingUrl() {
+    const urlDisplay = document.getElementById("url-display");
+    const no = document.getElementById("question-6-1");
+    const yes = document.getElementById("question-6-2");
+
+    if (no.checked) {
+        urlDisplay.style.display = "none";
+    } else if (yes.checked) {
+        urlDisplay.style.display = "block";
+    }
+}
+
+function otherDisplay() {
+    const other = document.getElementById("question-7-4");
+    const otherInput = document.getElementById("other-input");
+
+    if (other.checked) {
+        otherInput.style.display = "block";
+    } else {
+        otherInput.style.display = "none";
     }
 }
 
