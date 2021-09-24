8ba1aa1a60ea4d3ee0adefdd7c0bf6fa651c3ca9
Paul McAviney <87715498+paul-mcaviney@users.noreply.github.com>
2021-09-24 11:33:23 -0600

    a few changes to questionnaire
    auto resize text areas
    change text size of textareas
    testing form inputs

--- scripts.js
+++ scripts.js
@@ -89,4 +89,21 @@ function featureDisplay() {
 }
 
 
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+
+// For auto resizing textareas
+window.onload = function test() {
+const tx = document.getElementsByTagName("textarea");
+
+for (let i =0; i < tx.length; i++) {
+    tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+    tx[i].addEventListener("input", OnInput, false);
+}
+}
+
+function OnInput() {
+    this.style.height = "auto";
+    this.style.height = (this.scrollHeight) + "px";
+}
 
